26ddcebc24c46d13c83f0a9a5ed36af97ee5cf4b
shanedbutler <shanedbutler@gmail.com>
2022-11-03 13:27:56 -0400

    Remove un-used using statements

--- TabloidMVC/Controllers/TagController.cs
+++ TabloidMVC/Controllers/TagController.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.VisualBasic;
 using System;
-using System.Security.Claims;
 using TabloidMVC.Models;
-using TabloidMVC.Models.ViewModels;
 using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Controllers
--- TabloidMVC/Repositories/PostRepository.cs
+++ TabloidMVC/Repositories/PostRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Reflection.PortableExecutable;
+﻿using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using TabloidMVC.Models;
