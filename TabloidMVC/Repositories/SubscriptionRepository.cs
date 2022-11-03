2206aef2bf78559c6a53f80e8c13ce83e53999d2
TabathaSlater <taslater00@gmail.com>
2022-11-02 23:47:22 -0400

    Subscribe ticket

---
+++ TabloidMVC/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using TabloidMVC.Models;
+using TabloidMVC.Utils;
+
+namespace TabloidMVC.Repositories
+{
+    public class SubscriptionRepository : BaseRepository
+    {
+        public SubscriptionRepository(IConfiguration config) : base(config) { }
+
+
+
+    }
+}
