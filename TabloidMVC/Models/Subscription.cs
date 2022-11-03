3682a23a3749a67500ff74464357530dcdf3f9ec
TabathaSlater <taslater00@gmail.com>
2022-11-02 23:48:04 -0400

    subscribe tickets

--- TabloidMVC/Models/Subscription.cs
+++ TabloidMVC/Models/Subscription.cs
@@ -9,5 +9,7 @@ namespace TabloidMVC.Models
         public int ProviderUserProfileId { get; set; }
         public DateTime BeginDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
+        public int UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
     }
 }
